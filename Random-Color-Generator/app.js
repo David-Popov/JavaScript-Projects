a8e97221d35f81403eefbe22b76eb4dc3591ccc3
David-Popov <79060381+David-Popov@users.noreply.github.com>
2022-10-26 14:49:16 +0300

    Adding Color-Generator-Project

---
+++ Random-Color-Generator/app.js
@@ -0,0 +1,26 @@
+const input = document.getElementById('result');
+const button = document.getElementById('button');
+const copyBtn = document.getElementById('copy');
+const color = document.getElementById('color');
+let randomColor = '';
+console.log(color)
+
+button.addEventListener('click', e =>{
+    randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+    input.innerHTML =  randomColor.toLocaleUpperCase();
+    console.log(randomColor)
+    color.style.backgroundColor = randomColor;
+})
+
+copyBtn.addEventListener('click', e =>{
+    navigator.clipboard.writeText(randomColor.toLocaleUpperCase()).then(function() {
+        console.log('Async: Copying to clipboard was successful!');
+      }, function(err) {
+        console.error('Async: Could not copy text: ', err);
+      });
+})
+
+
+function myFunction() {
+    
+  }
